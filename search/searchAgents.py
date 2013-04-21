6410574f6041263078129a172ad11130358f6a59
unknown <Silvia@.(none)>
2013-04-21 19:00:31 +0200

    Prima possibile soluuzione al problema degli angoli, ancora non funzionante

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -275,18 +275,28 @@ class CornersProblem(search.SearchProblem):
       if not startingGameState.hasFood(*corner):
         print 'Warning: no food in corner ' + str(corner)
     self._expanded = 0 # Number of search nodes expanded
-    
+
     "*** YOUR CODE HERE ***"
+    self.statoIniziale = () #(posizioneAttuale, lista degli angoli visitati)
+    angoliVisitati = []
+    if self.startingPosition in self.corners:
+        angoliVisitati.append(self.startingPosition)
+    self.statoIniziale = (self.startingPosition, angoliVisitati)
+        
+       
+   
     
   def getStartState(self):
     "Returns the start state (in your state space, not the full Pacman state space)"
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return self.statoIniziale
+    #util.raiseNotDefined()
     
   def isGoalState(self, state):
     "Returns whether this search state is a goal state of the problem"
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return set(state[1]) == set(self.corners)
+    #util.raiseNotDefined()
        
   def getSuccessors(self, state):
     """
@@ -304,13 +314,20 @@ class CornersProblem(search.SearchProblem):
     for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
       # Add a successor state to the successor list if the action is legal
       # Here's a code snippet for figuring out whether a new position hits a wall:
-      #   x,y = currentPosition
-      #   dx, dy = Actions.directionToVector(action)
-      #   nextx, nexty = int(x + dx), int(y + dy)
-      #   hitsWall = self.walls[nextx][nexty]
-      
-      "*** YOUR CODE HERE ***"
-      
+         x,y = state[0]
+         dx, dy = Actions.directionToVector(action)
+         nextx, nexty = int(x + dx), int(y + dy)
+         hitsWall = self.walls[nextx][nexty]
+        
+         if not hitsWall:
+             prossimaPosizione = (nextx, nexty)
+             angoliVisitati = list(state[1])
+             if prossimaPosizione in self.corners:
+                 angoliVisitati.append(prossimaPosizione)
+                 costoAzione = self.getCostOfAction( [action] )
+                 successors.append( ( (prossimaPosizione, angoliVisitati), action, costoAzione) )
+                 
+                 "*** YOUR CODE HERE ***"
     self._expanded += 1
     return successors
 
